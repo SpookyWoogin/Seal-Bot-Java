803214ff1117c4ad222a8aeb91a07cc67afe840d
SpookyWoogin <cgideon360@gmail.com>
2022-05-18 11:06:15 -0700

    Initial Commit

---
+++ src/main/java/bot.java
@@ -0,0 +1,9 @@
+public class bot {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new bot().getGreeting());
+    }
+}
